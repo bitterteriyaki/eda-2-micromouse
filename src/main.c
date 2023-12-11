778f5de430ca7a8a041463589d423ac2baedd4f1
Caio <caiofeliperro@gmail.com>
2023-12-11 15:20:32 -0300

    main(debug): add debug mode

--- src/main.c
+++ src/main.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
+#ifdef DEBUG
+#define DEBUG_TEST 1
+#else
+#define DEBUG_TEST 0
+#endif
+
+#define debug(fmt, ...) \
+        do { if (DEBUG_TEST) fprintf(stderr, "%s:%d:%s(): " fmt, __FILE__, \
+                                __LINE__, __func__, __VA_ARGS__); } while (0)
+                                
 int main() {
     return 0;
 }
